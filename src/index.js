69177626a74f49654f99c05137c048725262c195
Aakanksha <82.25aakanksha@gmail.com>
2024-01-26 23:22:10 +0530

    PORT

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import connectDB from "./db/connectdb.js";
 dotenv.config({ path: "../.env" });
 
 connectDB().then(() => {
-  app.listen(process.env.port || 8000, () => {
-    console.log(`server is running in port ${process.env.port}`);
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`server is running in port ${process.env.PORT}`);
   });
 });
